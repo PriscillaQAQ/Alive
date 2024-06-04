d68a09b2e36b2d966df7050a6ab260b62b3f9fc1
Priscilla <14092888+priscillaqaq@user.noreply.gitee.com>
2024-06-04 16:52:52 +0800

    ling060401

---
+++ 页面/成就/achievement_resource.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name Achievement
+
+@export var id:String
+@export var name:String
+@export var date:String
+@export var classification:int
+@export var note:String
---
+++ 页面/日程管理/task_resource.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name Task
+
+@export var name:String
+@export var id:String
+@export var color:Color
+@export var date:String
+@export var start_time:String
+@export var end_time:String
+@export var description:String
+
