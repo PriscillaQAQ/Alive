26fe019af9b97cf2fadb34a9b5228e81f3733091
Priscilla <14092888+priscillaqaq@user.noreply.gitee.com>
2024-06-05 17:07:46 +0800

    little change in money-- ling

---
+++ 页面/攒钱/攒钱.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_添加_pressed():
+	pass # Replace with function body.
+
+
+func _on_纪录_pressed():
+	pass # Replace with function body.
