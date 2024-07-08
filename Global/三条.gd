d3dfd6a575f28a3a2fa3ad72fc54b0b258dcc556
Priscilla <131837096+PriscillaQAQ@users.noreply.github.com>
2024-07-08 09:27:35 +0800

    font

--- Global/三条.gd
+++ Global/三条.gd
@@ -3,6 +3,10 @@ extends GridContainer
 @onready var life = %"生命条"
 @onready var mood = %"心情条"
 @onready var iq = %"智力条"
+@onready var life_l = %"生命"
+@onready var mood_l = %"心情"
+@onready var iq_l = %"智力"
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +14,18 @@ func _ready():
 	life.value=GlobalVariables.life
 	mood.value=GlobalVariables.mood
 	iq.value=GlobalVariables.iq
+	check_figure_value(GlobalVariables.life,life_l)
+	check_figure_value(GlobalVariables.mood,mood_l)
+	check_figure_value(GlobalVariables.iq,iq_l)
+	
 	pass # Replace with function body.
+	
+func check_figure_value(value:int,label:Label):
+	var bad_font=load("res://assets/字体/LingKO゙SHIKKU-2.otf")
+	var good_font=load("res://assets/字体/1.ttf")
+	if value<=30:
+		label.add_theme_font_override("font",bad_font)
+	else:
+		label.add_theme_font_override("font",good_font)
 
 
